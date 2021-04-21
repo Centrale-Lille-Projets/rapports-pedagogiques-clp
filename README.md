2b542fc72f26b7080c3a9b7b2e087bfda00cba95
Cédric JUNG <cedced19@gmail.com>
2021-04-21 19:15:14 +0200

    Use template to generate Word files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # rapports-pedagogiques-clp
 Un programme pour générer un PDF permettant de présenter de manière visuelle les rapports pédagogiques.
+Pour l'utiliser ajouter un fichier `rapports.xlsx` à la racine.
+
+Après avoir installer nodejs, et copier ce fichier, il suffit d'éxécuter: 
+```
+npm install
+npm start
+```
---
+++ index.js
@@ -0,0 +1,51 @@
+const excelToJson = require('convert-excel-to-json');
+const fs = require('fs-extra');
+const moment = require('moment');
+const createReport = require('docx-templates').createReport;
+
+const result = excelToJson({
+    sourceFile: 'rapports.xlsx'
+});
+
+console.log('Liste des pages du Excel:');
+keys = Object.keys(result);
+for (i in keys) {
+    console.log('-' + keys[i]);
+}
+console.log('\n');
+
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+async function generate() {
+    fs.removeSync('result/');
+    fs.ensureDirSync('result/');
+    // Première page 
+    const firstPageTemplate = fs.readFileSync('template-RP.docx');
+    const firstPage = result.RP;
+    for (i in firstPage) {
+        if (i != 0) {
+
+            const real = firstPage[i];
+
+            for (i in letters) {
+                const letter = letters[i]
+                if (typeof real[letter] == 'undefined') {
+                    real[letter] = '';
+                }
+            }
+
+            real.formattedDate = moment(new Date(real.A)).format("DD/MM/YYYY");
+
+            const filename = `result/${real.B} ${real.C}.docx`;
+            const buffer = await createReport({
+                template: firstPageTemplate,
+                data: real,
+                cmdDelimiter: ['{', '}']
+            });
+
+            fs.writeFileSync(filename, buffer);
+        }
+    }
+
+}
+generate();
